7ee1ff18341ac8543eb3dcec68f55c6304142d8a
zvenzven <zvenzuehlsdorf@googlemail.com>
2019-04-01 14:52:56 +0200

    Add card edit and delete // cardflip frontend

--- routes/ideas.js
+++ routes/ideas.js
@@ -9,12 +9,32 @@ router.get('/getall', (req, res) => {
     .catch(err => res.json(err))
 })
 
-router.post('/create', (req, res) => {
-  console.log(req.body)
-  // req.body.id = uuidv1()
+router.get('/:id', (req, res) => {
+  Ideas.findById(req.params.id)
+    .then(idea => res.json(idea))
+    .catch(err => res.json(err))
+})
 
+router.post('/create', (req, res) => {
   Ideas.create(req.body)
     .then(idea => res.json(idea))
+    .catch(err => res.json(err))
+})
+
+router.delete('/:id', (req, res) => {
+  Ideas.findByIdAndDelete(req.params.id)
+    .then(idea => res.json(idea))
+    .catch(err => res.json(err))
+})
+
+router.patch('/:id', (req, res) => {
+  const id = req.params.id
+  console.log('im here, id: ' + id)
+  Ideas.findByIdAndUpdate(id, req.body, { new: true })
+    .then(idea => {
+      console.log(idea)
+      res.json(idea)
+    })
     .catch(err => res.json(err))
 })
 
